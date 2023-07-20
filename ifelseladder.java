59c08b1f9f8bfc27729e9110e20b5ad0b5df731f
Kalyankumarm <113247412+Kalyankumarm@users.noreply.github.com>
2023-07-20 08:51:42 +0530

    Create ifelseladder.java

---
+++ ifelseladder.java
@@ -0,0 +1,35 @@
+public class program
+{  
+    public static void main(String[] args) 
+    {  
+        int m=65;  
+        if(m<50)
+        {  
+        System.out.println("fail");  
+        }  
+        else if(m>=50 && m<60)
+        {  
+        System.out.println("D grade");  
+        }  
+        else if(m>=60 && m<70)
+        {  
+        System.out.println("C grade");  
+        }  
+        else if(m>=70 && m<80)
+        {  
+        System.out.println("B grade");  
+        }  
+         else if(m>=80 && m<90)
+        {  
+        System.out.println("A grade");  
+         } 
+        else if(m>=90 && m<100)
+        {  
+        System.out.println("A+ grade");  
+        }
+        else
+        {  
+        System.out.println("Invalid!");  
+        }  
+   }  
+} 
