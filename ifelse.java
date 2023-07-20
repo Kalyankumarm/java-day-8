29a2a7b6cfbda83f6d16e242bc6cec4246e896d8
Kalyankumarm <113247412+Kalyankumarm@users.noreply.github.com>
2023-07-20 08:45:44 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+public class ifelseprogram
+{ 
+    public static void main(String[] args) 
+    {  
+        int n=13;  
+        if(n%2==0)
+        {  
+            System.out.println("even number");  
+            
+        }  
+        else
+        {  
+            System.out.println("odd number");  
+        } 
+        
+    }  
+} 
